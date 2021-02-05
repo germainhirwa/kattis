ac3daa066541fdb322615669c8050e03ff928fa4
RussellDash332 <russellsaerang@gmail.com>
2021-02-05 20:33:43 +0800

    Much improved version of T9Spelling

--- src/T9 Spelling/T9Spelling3.java
+++ src/T9 Spelling/T9Spelling3.java
@@ -1,3 +1,5 @@
+// The most improved version of T9Spelling
+
 import java.io.*;
 import java.util.*;
 
@@ -8,52 +10,51 @@ public class T9Spelling3 {
         PrintWriter writer = new PrintWriter(System.out);
         int n = Integer.parseInt(sc.readLine());
 
-        Map<String, String> keys = new HashMap<String, String>();
-        keys.put("a","2");
-        keys.put("b","22");
-        keys.put("c","222");
-        keys.put("d","3");
-        keys.put("e","33");
-        keys.put("f","333");
-        keys.put("g","4");
-        keys.put("h","44");
-        keys.put("i","444");
-        keys.put("j","5");
-        keys.put("k","55");
-        keys.put("l","555");
-        keys.put("m","6");
-        keys.put("n","66");
-        keys.put("o","666");
-        keys.put("p","7");
-        keys.put("q","77");
-        keys.put("r","777");
-        keys.put("s","7777");
-        keys.put("t","8");
-        keys.put("u","88");
-        keys.put("v","888");
-        keys.put("w","9");
-        keys.put("x","99");
-        keys.put("y","999");
-        keys.put("z","9999");
-        keys.put(" ","0");
+        Map<Integer, Integer> keys = new HashMap<Integer, Integer>(); // create a HashMap
+        keys.put(97,2); // put every ASCII key and number, avoid String to improve time running
+        keys.put(98,22);
+        keys.put(99,222);
+        keys.put(100,3);
+        keys.put(101,33);
+        keys.put(102,333);
+        keys.put(103,4);
+        keys.put(104,44);
+        keys.put(105,444);
+        keys.put(106,5);
+        keys.put(107,55);
+        keys.put(108,555);
+        keys.put(109,6);
+        keys.put(110,66);
+        keys.put(111,666);
+        keys.put(112,7);
+        keys.put(113,77);
+        keys.put(114,777);
+        keys.put(115,7777);
+        keys.put(116,8);
+        keys.put(117,88);
+        keys.put(118,888);
+        keys.put(119,9);
+        keys.put(120,99);
+        keys.put(121,999);
+        keys.put(122,9999);
+        keys.put(32,0);
 
         for (int i = 1; i <= n; i++) {
             String word = sc.readLine();
-            String previous = " ";
+            int previous = -1; // set an impossible value
 
-            // Maybe separating them will help?
+            // Separating them will help to avoid concatenation
             writer.print("Case #");
             writer.print(i);
             writer.print(": ");
 
-            for (int j = 0; j < word.length(); j++) {
-                String wk = keys.get(word.substring(j,j+1)); // access the HashMap from j-th letter
-                if (previous.equals(wk.substring(0,1))) {
-                    writer.print(" "+wk);
-                } else {
-                    writer.print(wk);
+            for (int j = 0; j < word.length(); j++) { // iterate every letter in the word
+                int wk = keys.get((int) word.charAt(j)); // access the HashMap from j-th letter
+                if (previous == wk%10) { // compare the last digit because it's always the same as the first digit
+                    writer.print(" "); // add whitespace
                 }
-                previous = wk.substring(0,1);
+                writer.print(wk);
+                previous = wk%10; // replace previous
             }
             writer.println();
         }
