14bd56f2694dac05e358b453e3f1136532abbbc0
RussellDash332 <russellsaerang@gmail.com>
2021-02-13 15:17:03 +0800

    Update as of 13 February 2021

---
+++ src/ICPC Awards/icpcawards.py
@@ -0,0 +1,13 @@
+n = int(input())
+winners = []
+for _ in range(n):
+    u,t = input().split(" ")
+    flag = True
+    for i in range(len(winners)):
+        if winners[i][0] == u:
+            flag = False
+    if flag and len(winners) < 12:
+        winners.append([u,t])
+
+for k in range(12):
+    print(winners[k][0],winners[k][1])
