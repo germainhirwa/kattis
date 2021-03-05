8d6c60d5ef9350d1ce25587f4adafb17b1db3b16
RussellDash332 <russellsaerang@gmail.com>
2021-03-05 12:42:48 +0800

    Updated Conformity

--- src/Conformity/Conformity.java
+++ src/Conformity/Conformity.java
@@ -7,14 +7,32 @@ public class Conformity {
         BufferedReader sc = new BufferedReader(inp);
         PrintWriter writer = new PrintWriter(System.out);
 
-        Map<Integer,Integer> hm = new HashMap<Integer,Integer>();
+        Map<Long,Integer> hm = new HashMap<Long,Integer>();
         int maxPop = 0;
         int ans = 0;
         
         int n = Integer.parseInt(sc.readLine());
         for (int i = 0; i < n; i++) {
             String[] line = sc.readLine().split(" ");
-            int combination = 1;
+
+            /*
+            // A layman hashing function is to simply sort the list and use base to convert the list into integer/long
+            List<Integer> temp = new ArrayList<Integer>();
+            for (int j = 0; j < 5; j++) {
+                temp.add(Integer.parseInt(line[j]));
+            }
+            Collections.sort(temp);
+            
+            // Use base 400 (any big number can do)
+            long combination = 0L;
+            for (int j = 0; j < 5; j++) {
+                combination *= 400;
+                combination += temp.get(j)-100;
+            }
+            */
+
+            // This hashing function somehow works also in this case
+            long combination = 1L;
             for (int j = 0; j < 5; j++) {
                 combination *= (901-Integer.parseInt(line[j])); // random thought and number
             }
@@ -29,7 +47,7 @@ public class Conformity {
             }
         }
 
-        for (Integer c : hm.keySet()) {
+        for (Long c : hm.keySet()) {
             if (hm.get(c) == maxPop) {
                 ans += maxPop;
             }
