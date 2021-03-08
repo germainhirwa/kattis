32b33a37867e3b021b04da02d13841681599bb05
RussellDash332 <russellsaerang@gmail.com>
2021-03-08 18:49:53 +0800

    Deleted unnecessary methods in Reader class

--- src/Conformity/Conformity2.java
+++ src/Conformity/Conformity2.java
@@ -15,32 +15,6 @@ public class Conformity2 {
             bufferPointer = bytesRead = 0;
         }
  
-        public Reader(String file_name) throws IOException
-        {
-            din = new DataInputStream(
-                new FileInputStream(file_name));
-            buffer = new byte[BUFFER_SIZE];
-            bufferPointer = bytesRead = 0;
-        }
- 
-        public String readLine() throws IOException
-        {
-            byte[] buf = new byte[64]; // line length
-            int cnt = 0, c;
-            while ((c = read()) != -1) {
-                if (c == '\n') {
-                    if (cnt != 0) {
-                        break;
-                    }
-                    else {
-                        continue;
-                    }
-                }
-                buf[cnt++] = (byte)c;
-            }
-            return new String(buf, 0, cnt);
-        }
- 
         public int nextInt() throws IOException
         {
             int ret = 0;
@@ -60,48 +34,6 @@ public class Conformity2 {
             return ret;
         }
  
-        public long nextLong() throws IOException
-        {
-            long ret = 0;
-            byte c = read();
-            while (c <= ' ')
-                c = read();
-            boolean neg = (c == '-');
-            if (neg)
-                c = read();
-            do {
-                ret = ret * 10 + c - '0';
-            } while ((c = read()) >= '0' && c <= '9');
-            if (neg)
-                return -ret;
-            return ret;
-        }
- 
-        public double nextDouble() throws IOException
-        {
-            double ret = 0, div = 1;
-            byte c = read();
-            while (c <= ' ')
-                c = read();
-            boolean neg = (c == '-');
-            if (neg)
-                c = read();
- 
-            do {
-                ret = ret * 10 + c - '0';
-            } while ((c = read()) >= '0' && c <= '9');
- 
-            if (c == '.') {
-                while ((c = read()) >= '0' && c <= '9') {
-                    ret += (c - '0') / (div *= 10);
-                }
-            }
- 
-            if (neg)
-                return -ret;
-            return ret;
-        }
- 
         private void fillBuffer() throws IOException
         {
             bytesRead = din.read(buffer, bufferPointer = 0,
@@ -115,13 +47,6 @@ public class Conformity2 {
             if (bufferPointer == bytesRead)
                 fillBuffer();
             return buffer[bufferPointer++];
-        }
- 
-        public void close() throws IOException
-        {
-            if (din == null)
-                return;
-            din.close();
         }
     }
 
--- src/Shattered Cake/ShatteredCake2.java
+++ src/Shattered Cake/ShatteredCake2.java
@@ -1,3 +1,5 @@
+// Using Reader class to further improve I/O runtime
+
 import java.io.*;
 import java.util.*;
 
@@ -13,32 +15,6 @@ public class ShatteredCake2 {
             din = new DataInputStream(System.in);
             buffer = new byte[BUFFER_SIZE];
             bufferPointer = bytesRead = 0;
-        }
- 
-        public Reader(String file_name) throws IOException
-        {
-            din = new DataInputStream(
-                new FileInputStream(file_name));
-            buffer = new byte[BUFFER_SIZE];
-            bufferPointer = bytesRead = 0;
-        }
- 
-        public String readLine() throws IOException
-        {
-            byte[] buf = new byte[64]; // line length
-            int cnt = 0, c;
-            while ((c = read()) != -1) {
-                if (c == '\n') {
-                    if (cnt != 0) {
-                        break;
-                    }
-                    else {
-                        continue;
-                    }
-                }
-                buf[cnt++] = (byte)c;
-            }
-            return new String(buf, 0, cnt);
         }
  
         public int nextInt() throws IOException
@@ -60,48 +36,6 @@ public class ShatteredCake2 {
             return ret;
         }
  
-        public long nextLong() throws IOException
-        {
-            long ret = 0;
-            byte c = read();
-            while (c <= ' ')
-                c = read();
-            boolean neg = (c == '-');
-            if (neg)
-                c = read();
-            do {
-                ret = ret * 10 + c - '0';
-            } while ((c = read()) >= '0' && c <= '9');
-            if (neg)
-                return -ret;
-            return ret;
-        }
- 
-        public double nextDouble() throws IOException
-        {
-            double ret = 0, div = 1;
-            byte c = read();
-            while (c <= ' ')
-                c = read();
-            boolean neg = (c == '-');
-            if (neg)
-                c = read();
- 
-            do {
-                ret = ret * 10 + c - '0';
-            } while ((c = read()) >= '0' && c <= '9');
- 
-            if (c == '.') {
-                while ((c = read()) >= '0' && c <= '9') {
-                    ret += (c - '0') / (div *= 10);
-                }
-            }
- 
-            if (neg)
-                return -ret;
-            return ret;
-        }
- 
         private void fillBuffer() throws IOException
         {
             bytesRead = din.read(buffer, bufferPointer = 0,
@@ -115,13 +49,6 @@ public class ShatteredCake2 {
             if (bufferPointer == bytesRead)
                 fillBuffer();
             return buffer[bufferPointer++];
-        }
- 
-        public void close() throws IOException
-        {
-            if (din == null)
-                return;
-            din.close();
         }
     }
 
