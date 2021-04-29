e1fd0dc770ea14a72220b9fafddbcfc69b903c8e
RussellDash332 <russellsaerang@gmail.com>
2021-04-30 04:44:58 +0800

    Update as of 30 April 2021

---
+++ src/Heir's Dilemma/heirsdilemma.py
@@ -0,0 +1,24 @@
+def possible(n):
+    def helper(x,y):
+        if y == 0:
+            return True
+        elif y % 10 != 0 and x % (y%10) == 0:
+            return helper(x,y//10)
+        else:
+            return False
+    return helper(n,n)
+
+def distinct(n):
+    s = str(n)
+    d = {}
+    for v in s:
+        d[v] = d.get(v,0)+1
+    return len(s) == len(d)
+
+a,b = list(map(int,input().split(" ")))
+c = 0
+for i in range(a,b+1):
+    if distinct(i):
+        c += int(possible(i))
+
+print(c)
