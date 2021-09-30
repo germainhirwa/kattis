d35bed9d8f57fc3ccced07ae6df67db6cf72ad9b
RussellDash332 <russellsaerang@gmail.com>
2021-10-01 07:52:16 +0800

    Update as of 1 October 2021

---
+++ src/Bobby's Bet/bobby.py
@@ -0,0 +1,18 @@
+n = int(input())
+
+def choose(n, m):
+    if n <= 0 or m < 0:
+        return 0
+    if n == m or m == 0:
+        return 1 # not necessary but is fine
+    result = 1
+    for i in range(m):
+        result *= (n-i)/(i+1)
+    return int(result)
+
+for _ in range(n):
+    r, s, x, y, w = list(map(int, input().split(" ")))
+    p = 0
+    for i in range(x, y + 1):
+        p += choose(y, i) * ((s-r+1)/s)**(i) * (1-(s-r+1)/s)**(y-i)
+    print(["no", "yes"][int((w-1)*p > (1-p))])
---
+++ src/Incognito/incognito.py
@@ -0,0 +1,11 @@
+n = int(input())
+for _ in range(n):
+    k = int(input())
+    d = {}
+    for _ in range(k):
+        a, c = input().strip().split(" ")
+        d[c] = d.get(c, []) + [a]
+    ans = 1
+    for t in d:
+        ans *= len(set(d[t])) + 1
+    print(ans - 1)
