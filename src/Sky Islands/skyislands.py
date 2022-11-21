09c9ccd8aca2dcbb2fa33a1eddaa437e9c3307ce
RussellDash332 <russellsaerang@gmail.com>
2022-11-21 13:04:14 +0800

    Update as of 21 November 2022

---
+++ src/Sky Islands/skyislands.py
@@ -0,0 +1,31 @@
+class UFDS:
+    def __init__(self, N):
+        self.p = [i for i in range(N)]
+        self.rank = [0 for _ in range(N)]
+        self.num_sets = N
+
+    def find_set(self, i):
+        if self.p[i] == i: return i
+        self.p[i] = self.find_set(self.p[i])
+        return self.p[i]
+
+    def is_same_set(self, i, j):
+        return self.find_set(i) == self.find_set(j)
+
+    def union(self, i, j):
+        if not self.is_same_set(i, j):
+            self.num_sets -= 1
+            x, y = self.find_set(i), self.find_set(j)
+            if self.rank[x] > self.rank[y]:
+                self.p[y] = x
+            else:
+                self.p[x] = y
+                if self.rank[x] == self.rank[y]:
+                    self.rank[y] += 1
+
+n, m = map(int, input().split())
+u = UFDS(n)
+for _ in range(m):
+    a, b = map(int, input().split())
+    u.union(a-1, b-1)
+print(['NO', 'YES'][int(u.num_sets == 1)])
