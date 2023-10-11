8de39a1baef1a3e78c308ad83d27e0d7f75f8d0e
RussellDash332 <russellsaerang@gmail.com>
2023-10-11 16:45:58 +0800

    Update as of 11 October 2023

--- src/A Furious Cocktail/cocktail.py
+++
@@ -1,6 +0,0 @@
-import sys
-n, t = map(int, input().split())
-arr = sorted(map(int, sys.stdin))
-for i in range(n):
-    if arr[i]-t*i <= 0: print('NO'), sys.exit(0)
-print('YES')
--- src/Contingency Planning/contingencyplanning.py
+++
@@ -1,9 +0,0 @@
-n, f = int(input()), 1
-if n > 11: print('JUST RUN!!')
-else:
-    for _ in range(2, n+1): f *= _
-    c = f
-    for i in range(1, n):
-        f //= i
-        c += f
-    print(c)
